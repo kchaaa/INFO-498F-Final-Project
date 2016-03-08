b52045bf926feaedc729dbcefea13c8428f3e561
Nathan Yin <ryzr1320@gmail.com>
2016-03-08 15:10:32 -0800

    Started Visualizations

--- shiny/ui.R
+++ shiny/ui.R
@@ -1,4 +1,8 @@
 library(shiny)
+library(plotly)
+
+setwd("C:/Users/Administrator.UWIT-JAIE3J3AAH/Documents/Code/info498f/INFO-498F-Final-Project")
+data <- read.csv("data/Flint-Samples-Final.csv")
 
 flush <- colnames(data)[4:6]
 
