ab56f80fa416d51e352eae5536f5f7a1e2942b67
Pema <sherpa.pems@gmail.com>
2016-03-09 15:07:00 -0800

    edited

--- scripts/plot.r
+++ scripts/plot.r
@@ -2,7 +2,7 @@ library(dplyr)
 library(plotly)
 library(shiny)
 
-setwd("C:/Users/Administrator.UWIT-JAIE3J3AAH/Documents/Code/info498f/INFO-498F-Final-Project")
+setwd("/Users/Pema/info498f/INFO-498F-Final-Project")
 data <- read.csv("data/Flint-Samples-Final.csv")
 
 plot <- function(dataset, input1) {
