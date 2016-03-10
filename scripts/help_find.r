b5039263b010b5045c39d95ebdd1a95d37f0f4dd
Pema <sherpa.pems@gmail.com>
2016-03-09 18:11:55 -0800

    added the plot and the map of flint michigan filter locations

--- scripts/help_find.r
+++ scripts/help_find.r
@@ -1,6 +1,17 @@
 require(ggplot2)
 require(ggmap)
 require(maps)
+require(mapproj)
+require(sp)
 
-flint_sample <- read.csv("/Users/Pema/info498f/INFO-498F-Final-Project/data/Flint-SAMPLES-Final.csv")
-qmap('Flint', maptype = 'terrain')
+# Reads in the data of the filter locations in Flint
+filter_data <- read.csv("/Users/Pema/info498f/INFO-498F-Final-Project/data/Flint_Water_Filter_Locations.csv")
+
+# Gets the terrain map of Flint, Michigan
+map <- qmap('Flint', zoom = 12, maptype = 'terrain')
+
+# Converts the data of both lat/long from characters to numeric
+coords <- cbind(Longitude = as.numeric(as.character(filter_data$Longitude)), Latitude = as.numeric(as.character(filter_data$Latitude)))
+
+# Adds the points of the location of the water filters
+map + geom_point(data = filter_data, aes(filter_data$Latitude, filter_data$Longitude), color = "green", size = 10, alpha = 0.6, na.rm = TRUE)
