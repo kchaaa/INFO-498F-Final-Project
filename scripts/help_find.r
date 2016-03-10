1df2525860e06811662e54daecb0ba1662cf9774
Kevin Cha <kevinjcha96@gmail.com>
2016-03-09 21:13:17 -0800

    fixed index

--- scripts/help_find.r
+++ scripts/help_find.r
@@ -4,6 +4,8 @@ require(maps)
 require(mapproj)
 require(sp)
 
+filter_data <- read.csv('data/Flint_Water_Filter_Locations.csv')
+
 # Gets the terrain map of Flint, Michigan
 map <- qmap('Flint', zoom = 12, maptype = 'terrain')
 
