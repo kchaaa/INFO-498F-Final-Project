5bf72502d083ef2a1e0281cc12aa54d4c5abdf40
Pema <sherpa.pems@gmail.com>
2016-03-09 20:14:19 -0800

    fixed merge conflict error in server.R

--- shiny/server.R
+++ shiny/server.R
@@ -4,16 +4,6 @@ library(dplyr)
 library(ggplot2)
 library(ggmap)
 
-<<<<<<< HEAD
-source("/Users/Pema/info498f/INFO-498F-Final-Project/scripts/help_find.R")
-
-filter_loc <- read.csv("/Users/Pema/info498f/INFO-498F-Final-Project/data/Flint_Water_Filter_Locations.csv")
-=======
-source("c:/Users/Administrator.UWIT-JAIE3J3AAH/Documents/Code/info498f/INFO-498F-Final-Project/scripts")
-
-filter_loc <- read.csv("C:/Users/Administrator.UWIT-JAIE3J3AAH/Documents/Code/info498f/INFO-498F-Final-Project/data/Flint_Water_Filter_Locations.csv")
->>>>>>> 0296d42574afd4261fd9cda1b6a8932bf38d5527
-
 shinyServer(
   function(input, output) {
     output$plot1 <- renderPlot({
