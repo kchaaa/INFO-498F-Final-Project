a4f280e7ffcb15650bc8236bbb5437b49c08a470
Pema <sherpa.pems@gmail.com>
2016-03-09 18:14:30 -0800

    added a variable get_filter_map in help_find code for accessibility

--- scripts/help_find.r
+++ scripts/help_find.r
@@ -14,4 +14,4 @@ map <- qmap('Flint', zoom = 12, maptype = 'terrain')
 coords <- cbind(Longitude = as.numeric(as.character(filter_data$Longitude)), Latitude = as.numeric(as.character(filter_data$Latitude)))
 
 # Adds the points of the location of the water filters
-map + geom_point(data = filter_data, aes(filter_data$Latitude, filter_data$Longitude), color = "darkblue", size = 10, alpha = 0.8, na.rm = TRUE)
+get_filter_map <- map + geom_point(data = filter_data, aes(filter_data$Latitude, filter_data$Longitude), color = "darkblue", size = 10, alpha = 0.8, na.rm = TRUE)
