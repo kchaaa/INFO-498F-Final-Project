c3471e86171ff697b158429386a0e56548ef420d
Nathan Yin <ryzr1320@gmail.com>
2016-03-09 17:36:01 -0800

    Added dataframe that holds number of houses above limit

--- scripts/plot.r
+++ scripts/plot.r
@@ -8,8 +8,21 @@ plot <- function(dataset, input1) {
 }
 
 plot2 <- function(dataset) {
-  Wards <- dataset[,3]
-  return(plot_ly(data = dataset, x = dataset[,4], y = dataset[,6], color = Wards, type = "scatter", mode = "markers") %>% 
+  return(plot_ly(data = dataset, x = dataset[,4], y = dataset[,6], color = dataset[,3], type = "scatter", mode = "markers") %>% 
            layout(xaxis = list(title = "Pb Level Pre-Flush"), yaxis = list(title = "Pb Level 2 Min After")))
-  }
+}
+
+stackbar <- plot_ly(
+  x = warning_levels[,3],
+  y = warning_levels[,6],
+  name = "Warning Level",
+  type = "bar"
+)
+stackbar2 <- add_trace(
+  p,
+  x = above_epa[,3],
+  y = above_epa[,6],
+  name = "Action Level"
+) %>% 
+  layout(barmode = "stack")
 
