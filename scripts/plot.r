4d20d6afc3a5bdc58b3ce0f97315b84861a8408f
Nathan Yin <ryzr1320@gmail.com>
2016-03-10 14:09:48 -0800

    Commented parts of index.rmd and plot.r

--- scripts/plot.r
+++ scripts/plot.r
@@ -1,41 +1,29 @@
+# Requires packages to be used in script
 library(dplyr)
 library(plotly)
 library(shiny)
 
-plot <- function(dataset, input1) {
-  return(plot_ly(data = dataset, 
-                 x = data[,3], 
-                 y = paste0(dataset[,input1]), 
-                 type = "bar") %>% 
-           layout(xaxis = list(title = "Wards"), 
-                  yaxis = list(title = input1)))  
-}
-
-plot2 <- function(dataset) {
-  return(plot_ly(data = dataset, 
-                 x = dataset[,4], 
-                 y = dataset[,6], 
-                 color = dataset[,3], 
-                 type = "scatter", 
-                 mode = "markers") %>% 
-           layout(xaxis = list(title = "Pb Level Pre-Flush"), 
-                  yaxis = list(title = "Pb Level 2 Min After")))
-}
-
+# Saves graph into a function to be used in the rmd
+# And takes in three different dataframes to be used in it
 stackbar <- function(data1, data2, data3, name) { 
+  # First section of the chart that uses the safe_num dataset
   return(plot_ly(x = data1[, 1],
                  y = data1[, name],
                  name = "Safe Level",
                  type = "bar",
-                 marker = list(color = toRGB("springgreen4"))) %>% 
+                 marker = list(color = toRGB("springgreen4"))) %>%
+       # Second section that takes in the warning_num dataset   
        add_trace(x = data2[, 1],
                  y = data2[, name],
                  name = "Warning Level",
-                 marker = list(color = toRGB("darkorange"))) %>% 
+                 marker = list(color = toRGB("darkorange"))) %>%
+       # Third section that takes in the abovee_num dataset
        add_trace(x = data3[, 1],
                  y = data3[, name],
                  name = "Danger Level",
                  marker = list(color = toRGB("firebrick2"))) %>% 
+          # Adds a layout function that names the axis of the graph
+          # As wells as stacks the three different sections into one stacked bar chart
           layout(xaxis = list(title = "Wards"),
                  yaxis = list(title = "Number of Houses"),
                  barmode = "stack"))
